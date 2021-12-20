7ac60c19cd5b60ceb84787e85be85d7cf3393a62
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-12-20 09:47:56 +0900

    Add the title section

--- four-card-feature/src/App.svelte
+++ four-card-feature/src/App.svelte
@@ -1,9 +1,26 @@
+<script lang="ts">
+    import Title from "./components/Title.svelte";
+    import Section from "./components/Section.svelte";
+</script>
+
 <svelte:head>
     <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;600&display=swap"
         rel="stylesheet"
     />
 </svelte:head>
-<div id="app" />
+<div id="app">
+    <Title />
+    <Section />
+</div>
+
+<style>
+    #app {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
---
+++ four-card-feature/src/components/Title.svelte
@@ -0,0 +1,31 @@
+<div id="title">
+    <h1>
+        Reliable, efficienct delivery <span class="emphasized"
+            >Powered by Technology</span
+        >
+    </h1>
+    <p>
+        Our Artificial Intelligence powered tools use millions of project data
+        points to ensure that your project is successful
+    </p>
+</div>
+
+<style>
+    #title {
+        width: 500px;
+    }
+    #title > h1 {
+        color: rgb(80, 80, 100);
+        font-weight: 200;
+        text-align: center;
+        margin-bottom: 20px;
+    }
+    #title > p {
+        color: rgb(170, 170, 170);
+        text-align: center;
+        line-height: 1.5;
+    }
+    .emphasized {
+        font-weight: 600;
+    }
+</style>
